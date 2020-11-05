b073272bd6a08c3dda80f1fd837bd842d5326630
snaejui95 <9643100866@mail.ru>
2020-11-05 11:50:10 +0300

    controlwork

---
+++ controlwork/date/date.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+int main()
+{
+	struct tm* d;
+	char date[20];
+	const time_t t = time(NULL);
+	d = localtime(&t);
+	strftime(date, 20, "%d.%m.%Y", d);
+	printf("%s\n", d);
+	system("pause");
+	return 0;
+}
