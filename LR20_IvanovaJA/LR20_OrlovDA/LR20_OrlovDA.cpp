7c898dd4d11a136b68c9cc8afed9b6be542ffd2f
snaejui95 <9643100866@mail.ru>
2020-11-08 22:02:11 +0300

    ЛР по СП

---
+++ LR20_IvanovaJA/LR20_OrlovDA/LR20_OrlovDA.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "MyMathFuncs.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int r;
+    cout << "Введите радиус: ";
+    cin >> r;
+    cout << MathFuncs::MyMathFuncs::Perimetr(r) << endl;
+    cout << MathFuncs::MyMathFuncs::Square(r);
+}
